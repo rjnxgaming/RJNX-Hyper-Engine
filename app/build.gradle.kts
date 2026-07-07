6301a033a48550422c849f94ec9db141807c5dc5
rjnxgaming <rjnxgaming@gmail.com>
2026-07-07 19:52:51 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,3 +2,35 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
+android {
+    namespace = "com.rjnx.hyperengine"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rjnx.hyperengine"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+dependencies {
+
+}
